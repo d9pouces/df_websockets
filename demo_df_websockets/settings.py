1b6e35d1b4a198bf5a77d6212786c6688f51d17d
d9pouces <github@19pouces.net>
2021-03-25 20:08:29 +0100

    start of a demo app for testing all JS functions

--- demo_df_websockets/settings.py
+++ demo_df_websockets/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = "demo_df_websockets.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
 
