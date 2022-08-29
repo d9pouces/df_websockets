a48a699c27ec3612b58323612e5704e14b3dcc17
d9pouces <github@19pouces.net>
2022-08-29 17:25:47 +0200

    before testing

--- df_websockets/__init__.py
+++ df_websockets/__init__.py
@@ -13,4 +13,4 @@
 #  or https://cecill.info/licences/Licence_CeCILL-B_V1-fr.txt (French)         #
 #                                                                              #
 # ##############################################################################
-__version__ = "1.0.0"
+__version__ = "1.0.0b1"
